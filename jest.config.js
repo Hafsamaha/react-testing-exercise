7e7b519b85947caff25b29cd2589de9616c510d2
Hafsamaha <sidilotfi@arcor.de>
2024-06-10 17:28:13 +0200

    react test durchgeführt

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest', 
+    },
+    transformIgnorePatterns: [
+      'node_modules/(?!some-es-module)', 
+    ],
+  };
+  
